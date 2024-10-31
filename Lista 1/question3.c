329274162b937ba1aa493ced64c2cdc80c94720d
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-10-31 11:02:36 +0000

    declaração dos funções

--- Lista 1/question3.c
+++ Lista 1/question3.c
@@ -10,3 +10,9 @@ typedef struct Conta{
     char telefone[15];
     float saldo;    
 } Conta;
+int buscarConta(Conta contas[],int numConta,int numeroConta);
+void cadastrarConta(Conta contas[], int *numConta);
+void consultarSaldo(Conta contas[],int nConta,int numConta);
+void fazerDeposito(Conta contas[],int numConta, int nConta);
+void fazerSaque(Conta contas[],int conta, int conta1);
+void exibirConta(Conta contas[], int conta);
