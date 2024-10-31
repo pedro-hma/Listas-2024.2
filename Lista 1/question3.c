2de66eaaf31dc4f767ec6938ebffbd71eea08a43
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-10-31 11:16:45 +0000

    introdução da main

--- Lista 1/question3.c
+++ Lista 1/question3.c
@@ -16,3 +16,22 @@ void consultarSaldo(Conta contas[],int nConta,int numConta);
 void fazerDeposito(Conta contas[],int numConta, int nConta);
 void fazerSaque(Conta contas[],int conta, int conta1);
 void exibirConta(Conta contas[], int conta);
+
+int main(){
+    Conta contas [MAX_CONTA];
+    int numContas = 0;
+    int numConta = 0;
+    int conta = 0;
+    int contas = 0;
+
+    int aux = buscarConta(contas,numContas,12345);
+    printf("Resultado da busca %d\n",aux);
+
+    cadastrarConta(contas,&numContas);
+    consultarSaldo(contas,numContas,contas[0].numeroConta);
+    fazerDeposito(contas,numContas,contas[0].numeroConta);
+    fazerSaque(contas,conta,contas[0].numeroConta);
+    exibirConta(contas,contas);
+
+    return 0;
+}
