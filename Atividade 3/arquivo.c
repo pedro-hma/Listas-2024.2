77e87abc8c6f7da253836f4e97786fdfa9d88338
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-11-12 18:25:15 +0000

    menu

--- Atividade 3/arquivo.c
+++ Atividade 3/arquivo.c
@@ -14,14 +14,32 @@ struct VIDEO{
     char categoria [MAX_CATEGORIA];
     int duracao;
 } VIDEO;
+
 struct CADASTROUSUARIO{
     int id;
     char nome[MAX_NOME];
     int favoritos;
     int videosfavaritos;
 } CADASTROUSUARIO;
+
 void cadastrarvideo();
 void listarvideo();
 void cadastrarusuario();
 void listarusuario();
 void removerusuario();
+
+int main(){
+    int opcao,id;
+
+    while(1){
+        printf("\nMenu:\n");
+        printf("1. Cadastrar vídeo\n");
+        printf("2. Cadastrar usuário\n");
+        printf("3. Listar vídeo\n");
+        printf("4. Listar usuário\n");
+        printf("5. Remover usuário\n");
+        printf("6. Sair\n");
+        printf("Escolha uma opção \n");
+        scanf("%d",&opcao);
+    }
+}
