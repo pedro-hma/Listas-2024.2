ded8e8f919dfdf08b34ba36d6bff23cb18253bde
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-11-12 17:58:50 +0000

    início

---
+++ Atividade 3/arquivo.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+# define MAX_NOME 50
+# define MAX_DESCRIÇÃO 100
+# define MAX_CATEGORIA 50
+# define MAX_VIDEOS 20
+
+struct VIDEO{
+    int id;
+    char titulo [MAX_NOME];
+    char descricao [MAX_DESCRIÇÃO];
+    char categoria [MAX_CATEGORIA];
+    int duracao;
+} VIDEO;
+struct CADASTROUSUARIO{
+    int id;
+    char nome[MAX_NOME];
+    int favoritos;
+    int videosfavaritos;
+} CADASTROUSUARIO;
